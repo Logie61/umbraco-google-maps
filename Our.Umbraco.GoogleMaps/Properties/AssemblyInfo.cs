8a8a7ff7138a14477f295b822e36ead3ee0b58a0
vertino <none@none>
2012-04-03 19:29:08 +0100

    Bumped version number - v2.0.4

--- Our.Umbraco.GoogleMaps/Properties/AssemblyInfo.cs
+++ Our.Umbraco.GoogleMaps/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Umbraco Community")]
 [assembly: AssemblyProduct("Our.Umbraco.GoogleMaps")]
-[assembly: AssemblyCopyright("Copyright \xa9 The Umbraco Community 2011")]
+[assembly: AssemblyCopyright("Copyright \xa9 The Umbraco Community 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 // Major Version, Minor Version, Build Number, Revision
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.3.*")]
-// [assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyVersion("2.0.4.*")]
+// [assembly: AssemblyFileVersion("2.0.4.0")]
