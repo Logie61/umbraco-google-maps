bbd4f54a008e69080ecc139dd89d7d82d56f42bb
vertino <none@none>
2012-09-12 07:50:21 +0100

    Modified MSBuild script to handle version number generation.

--- Our.Umbraco.GoogleMaps/Properties/AssemblyInfo.cs
+++ Our.Umbraco.GoogleMaps/Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("Our.Umbraco.GoogleMaps")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -12,15 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible to COM components.
-// If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("57E4AED4-217D-47AF-A306-87BE990752E2")]
-
-// Version information for an assembly consists of the following four values:
-// Major Version, Minor Version, Build Number, Revision
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.5.*")]
-// [assembly: AssemblyFileVersion("2.0.5.0")]
