73e9e958717fcd0deaef8c7a0058ba36f6d64b13
vertino <none@none>
2013-09-12 23:12:16 +0100

    Bumped the version number to v2.1.0 - since there are new features. Removed the v2.0.7 tag.

--- Our.Umbraco.GoogleMaps/Properties/VersionInfo.cs
+++ Our.Umbraco.GoogleMaps/Properties/VersionInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.7")]
-[assembly: AssemblyFileVersion("2.0.1538.49851")]
-[assembly: AssemblyInformationalVersion("2.0.7-build1538")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.1540.63305")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
 
 
